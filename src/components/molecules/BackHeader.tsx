b3c76cee20ee7d1617e3ae50d38c3f84b1c159a7
James Mah <jamesmah@outlook.com>
2021-10-15 19:08:39 +1100

    update browser back functionality

--- src/components/molecules/BackHeader.tsx
+++ src/components/molecules/BackHeader.tsx
@@ -1,10 +1,12 @@
-import { useHistory } from "react-router-dom"
+import { Link, useHistory } from "react-router-dom"
 import { css } from "styled-components"
 import { Breakpoint } from "../../theme"
 import Container from "../elements/Container"
 
 const BackHeader = ({ width }: { width?: Breakpoint }) => {
   const history = useHistory()
+  const useBrowserBack = history.action !== `POP`
+
   return (
     <Container
       $width={width}
@@ -13,21 +15,39 @@ const BackHeader = ({ width }: { width?: Breakpoint }) => {
       `}
       data-aos={`fade`}
     >
-      <div
-        onClick={history.goBack}
-        css={css`
-          margin: 1rem 0 2rem;
-          cursor: pointer;
-          display: flex;
-          align-items: center;
+      {useBrowserBack ? (
+        <div
+          onClick={history.goBack}
+          css={css`
+            margin: 1rem 0 2rem;
+            cursor: pointer;
+            display: flex;
+            align-items: center;
 
-          :hover {
-            color: ${({ theme }) => theme.palette.light[1]};
-          }
-        `}
-      >
-        ❮ Back
-      </div>
+            :hover {
+              color: ${({ theme }) => theme.palette.light[1]};
+            }
+          `}
+        >
+          ❮ Back
+        </div>
+      ) : (
+        <Link
+          to={`/blog`}
+          css={css`
+            margin: 1rem 0 2rem;
+            cursor: pointer;
+            display: flex;
+            align-items: center;
+
+            :hover {
+              color: ${({ theme }) => theme.palette.light[1]};
+            }
+          `}
+        >
+          ❮ Back
+        </Link>
+      )}
     </Container>
   )
 }
