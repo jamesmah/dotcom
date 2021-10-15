5338ac739c9f77330ec6f9df95f3d77ce736ed6b
James Mah <jamesmah@outlook.com>
2021-10-15 18:49:52 +1100

    Updates

--- static/js/9.1799e930.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpdotcom=this.webpackJsonpdotcom||[]).push([[9],{72:function(t,e,o){"use strict";o.r(e);var n=o(0);e.default=function(){return Object(n.jsx)(n.Fragment,{children:"Welcome to the first post in my website blog. Hope to see you again in the future."})}}}]);
-//# sourceMappingURL=9.1799e930.chunk.js.map
