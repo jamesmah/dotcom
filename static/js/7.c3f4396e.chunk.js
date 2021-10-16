2e4f1e1491d28376400b243155d2a9154e022537
James Mah <jamesmah@outlook.com>
2021-10-16 18:31:29 +1100

    Updates

--- static/js/7.c3f4396e.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpdotcom=this.webpackJsonpdotcom||[]).push([[7],{86:function(t,e,n){"use strict";n.r(e);var o=n(2),c=n(11),i=n(0);e.default=function(){return Object(i.jsx)(a,{children:Object(i.jsx)("h1",{"data-aos":"fade-up",children:"Coming Soon..."})})};var a=Object(o.c)(c.a).withConfig({componentId:"sc-18n3hp4-0"})(["flex-grow:1;display:flex;flex-direction:column;justify-content:center;text-align:center;padding-bottom:5rem;"])}}]);
-//# sourceMappingURL=7.c3f4396e.chunk.js.map
