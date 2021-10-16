645ae5f055510bc4e78599f7e1a181ca86f8f637
James Mah <jamesmah@outlook.com>
2021-10-16 19:46:08 +1100

    hide none category

--- src/components/molecules/CategoryPills.tsx
+++ src/components/molecules/CategoryPills.tsx
@@ -6,25 +6,26 @@ import Stack from "../elements/Stack"
 const CategoryPills = ({ categories, ...props }: { categories: string[] }) => {
   return (
     <Stack direction={`row`} gap={`1rem`}>
-      {categories.map((category, index, array) => (
-        <div
-          {...props}
-          key={index}
-          data-aos={`fade`}
-          data-aos-delay={(array.length - index - 1) * 100}
-          css={css`
-            color: ${({ theme }) => theme.palette.light[2]};
-            background: ${({ theme }) => theme.palette.dark[2]};
-            font-size: 0.75rem;
-            padding: 0.125rem 0.375rem;
-            border-radius: 0.5rem;
-            font-weight: 600;
-          `}
-        >
-          {category !== Category.None &&
-            (safeGetCategoryKey(category) || category)}
-        </div>
-      ))}
+      {categories
+        .filter((category) => category && category !== Category.None)
+        .map((category, index, array) => (
+          <div
+            {...props}
+            key={index}
+            data-aos={`fade`}
+            data-aos-delay={(array.length - index - 1) * 100}
+            css={css`
+              color: ${({ theme }) => theme.palette.light[2]};
+              background: ${({ theme }) => theme.palette.dark[2]};
+              font-size: 0.75rem;
+              padding: 0.125rem 0.375rem;
+              border-radius: 0.5rem;
+              font-weight: 600;
+            `}
+          >
+            {safeGetCategoryKey(category) || category}
+          </div>
+        ))}
     </Stack>
   )
 }
