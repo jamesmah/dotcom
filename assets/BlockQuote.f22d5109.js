14ca64436d85646676049cf4d18651ccb3efa82e
James Mah <jamesmah@outlook.com>
2021-12-17 09:21:56 +1100

    Updates

---
+++ assets/BlockQuote.f22d5109.js
@@ -0,0 +1,1 @@
+import{_ as o,b as t,j as e}from"./vendor.13c5f106.js";const d=({quote:a,originator:s})=>t("blockquote",{"data-aos":"fade-right",children:[e("q",{children:a}),e(n,{children:s&&` - ${s}`})]});var n=o("span").withConfig({componentId:"sc-1ntv5kk-0"})(["white-space:nowrap;"]);export{d as B};
---
+++ assets/ExternalLink.00a52d25.js
@@ -0,0 +1,1 @@
+import{b as e,F as s}from"./vendor.13c5f106.js";const l=({href:a,icon:r="\u{1F517}",children:n})=>e("a",{href:a,rel:"noreferrer",target:"_blank",children:[r&&e(s,{children:[r,"\xA0"]}),n]});export{l as E};
