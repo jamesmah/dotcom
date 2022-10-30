98497680f8a34411dce33e9caf89c86a80639502
James Mah <jamesmah@outlook.com>
2022-10-31 10:18:09 +1100

    Updates

--- assets/ComingSoon.c806eb39.js
+++
@@ -1,1 +0,0 @@
-import{j as n,_ as o}from"./vendor.17de887c.js";import{C as e}from"./index.c53404a0.js";const r=()=>n(t,{children:n("h1",{"data-aos":"fade-up",children:"Coming Soon..."})});var t=o(e).withConfig({componentId:"sc-18n3hp4-0"})(["flex-grow:1;display:flex;flex-direction:column;justify-content:center;text-align:center;padding-bottom:5rem;"]);export{r as default};
--- assets/PageContainer.0e88be32.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,j as e}from"./vendor.17de887c.js";import{S as o,f as n}from"./index.c53404a0.js";const d=({children:a})=>e(s,{gap:"1rem",children:a});var s=t(o).withConfig({componentId:"sc-1gt456-0"})(["padding-top:1rem;animation:"," 1s ease;"],n);export{d as P};
