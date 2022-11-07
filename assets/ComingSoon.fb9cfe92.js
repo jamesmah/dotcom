6ceb72b1c17d518d1a45c79cd2ee7e0a1bce9a33
James Mah <jamesmah@outlook.com>
2022-11-08 08:07:04 +1100

    Updates

---
+++ assets/ComingSoon.fb9cfe92.js
@@ -0,0 +1,1 @@
+import{j as n,_ as o}from"./vendor.17de887c.js";import{C as e}from"./index.7490b305.js";const a=()=>n(t,{children:n("h1",{"data-aos":"fade-up",children:"Coming Soon..."})});var t=o(e).withConfig({componentId:"sc-18n3hp4-0"})(["flex-grow:1;display:flex;flex-direction:column;justify-content:center;text-align:center;padding-bottom:5rem;"]);export{a as default};
