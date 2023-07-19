428ec45e4bd79fd866b10d2cbd9922123fde345f
James Mah <jamesmah@outlook.com>
2023-07-20 07:00:36 +1000

    remove dev content

--- src/content/allArticles.ts
+++ src/content/allArticles.ts
@@ -1,8 +1,9 @@
-import { ArticleData } from "../types"
+import { ArticleData, Tag } from "../types"
 
 const modules = import.meta.globEager(`./**/index.tsx`)
 const articles: ArticleData[] = Object.values(modules)
   .map((m) => m.default)
+  .filter((a: ArticleData) => !a.tags.includes(Tag.Dev))
   .sort((a, b) => b.timeStamp.getTime() - a.timeStamp.getTime())
 
 export const allArticles = articles
